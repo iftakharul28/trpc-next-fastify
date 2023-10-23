eb3ef7ebbae2beef83da80fcdc851d2866cdd078
iftakharul28 <iftakharul28@gmail.com>
2023-10-24 01:06:46 +0600

    auth model updated

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
-import { getUserAuth } from '../lib/auth/utils';
+import AuthForm from '@frontend/src/components/auth/Form';
+import { getUserAuth } from '@frontend/src/lib/auth/utils';
 import { trpc } from '../lib/trpc';
 
 import { redirect } from 'next/navigation';
@@ -12,6 +13,7 @@ export default async function Home() {
     <div className='max-w-5xl mx-auto md:p-0 p-6'>
       <pre className='bg-slate-100 p-6 rounded-lg my-2'>{JSON.stringify(response, null, 2)}</pre>
       <pre className='bg-slate-100 p-6 rounded-lg my-2'>{JSON.stringify(user, null, 2)}</pre>
+      <AuthForm action='/api/auth/sign-out' />
     </div>
   );
 }
